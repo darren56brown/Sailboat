f1c9374147818c6e2daeb749ee0648007d664358
Darren <darren56brown@gmail.com>
2026-03-01 20:13:45 -0500

    physics changes

--- boat.gd
+++ boat.gd
@@ -1,17 +1,20 @@
 extends RigidBody2D
 
-@export var wind_velocity = Vector2(200, 0)
-@export var rudder_rate_deg_per_frame = 120.0
+@export var wind_velocity = Vector2(150, 0)
+@export var rudder_rotation_rate = 120.0 #deg/sec
 @export var max_rudder_angle_deg = 60.0
 @export var rudder_efficiency = 2.0
+@export var rudder_hinge_position = Vector2(-18, 0)
+@export var rudder_length = 10
+@export var water_velocity = Vector2(0, 5)
 
 @onready var rudder_pivot = $RudderPivot
 @onready var rudder_arrow = $RudderForceArrow
 
 var rudder_angle_deg = 0.0
 var is_centering: bool = false
-var rudder_force_local = Vector2()
-var rudder_center_local = Vector2()
+var rudder_force = Vector2()
+var rudder_center_position = Vector2()
 
 #Computed as often as possible. Use for graphics updates and to recore from UI
 func _process(delta: float):
@@ -26,8 +29,8 @@ func _process(delta: float):
 	rudder_pivot.rotation = deg_to_rad(rudder_angle_deg)
 	
 	#Update rudder force arrow graphics
-	rudder_arrow.position = rudder_center_local
-	rudder_arrow.set_point_position(1, rudder_force_local * -0.5)
+	rudder_arrow.position = rudder_center_position
+	rudder_arrow.set_point_position(1, rudder_force * -0.5)
 	
 #Computed every physics tick. Do everything here except set state varaibles
 func _physics_process(delta: float):
@@ -37,44 +40,38 @@ func _physics_process(delta: float):
 		
 	#Don't use built in physics model for rudder angle
 	if is_centering:
-		rudder_angle_deg = move_toward(rudder_angle_deg, 0, rudder_rate_deg_per_frame * delta)
+		rudder_angle_deg = move_toward(rudder_angle_deg, 0, rudder_rotation_rate * delta)
 	else:
 		var turn_input = Input.get_axis("steer_right", "steer_left")
 		if turn_input != 0:
-			rudder_angle_deg = clamp(rudder_angle_deg + (turn_input * rudder_rate_deg_per_frame * delta),
+			rudder_angle_deg = clamp(rudder_angle_deg + (turn_input * rudder_rotation_rate * delta),
 				-max_rudder_angle_deg, max_rudder_angle_deg)
+				
+	var rudder_unit_vec = Vector2.LEFT.rotated(deg_to_rad(rudder_angle_deg))
+	rudder_center_position = rudder_hinge_position + rudder_unit_vec * rudder_length / 2.0
 		
 #Also computed every physics tick. Update state variables here
 func _integrate_forces(state):
-	var stern_offset_local = Vector2(-18, 0) # The hinge point
-	var rudder_length = 10.0                 # The length of your rudder blade
-	var center_offset = rudder_length / 2.0  # Center of pressure is halfway
+	var velocity_vector = state.linear_velocity
+	velocity_vector = velocity_vector.rotated(-rotation)
 	
-	# Calculate where the MIDDLE of the rudder is in boat-local space
-	var rudder_dir_local = Vector2.RIGHT.rotated(deg_to_rad(rudder_angle_deg))
-	rudder_center_local = stern_offset_local - (rudder_dir_local * center_offset)
+	var rudder_unit_normal = Vector2.UP.rotated(deg_to_rad(rudder_angle_deg))
+	var water_velocity_loc = water_velocity.rotated(-rotation)
 	
-	var global_vel = state.linear_velocity
-	var local_vel = global_vel.rotated(-rotation)
+	var flow_velocity = velocity_vector - water_velocity_loc
+	var rudder_normal_velocity = rudder_unit_normal * flow_velocity.dot(rudder_unit_normal)
 	
-	# A. Get the "Face" (Normal) of the rudder
-	var rudder_normal_local = Vector2.UP.rotated(deg_to_rad(rudder_angle_deg))
-	
-	# B. Calculate Pressure
-	var flow_pressure = local_vel.dot(rudder_normal_local)
-	
-	# C. Resulting Force
-	rudder_force_local = rudder_normal_local * flow_pressure * -rudder_efficiency
-	
-	# Apply force at the ACTUAL CENTER of the rudder, not the hinge
-	# We rotate the local center point into global space
-	var force_pos_global = rudder_center_local.rotated(rotation)
-	apply_force(rudder_force_local.rotated(rotation), force_pos_global)
+	rudder_force = rudder_normal_velocity * -rudder_efficiency
+	apply_force(rudder_force.rotated(rotation),
+		rudder_center_position.rotated(rotation))
 
-	# --- 4. HYDRODYNAMICS (Tracking) ---
-	local_vel.x *= 0.99  # Forward glide
-	local_vel.y *= 0.85  # Lateral resistance
-	state.linear_velocity = local_vel.rotated(rotation)
+	#reduce flow velocity due to drag
+	flow_velocity.x *= 0.95  # Forward glide
+	flow_velocity.y *= 0.45  # Lateral resistance
+	
+	#compute new velocity vector and apply it
+	velocity_vector = flow_velocity + water_velocity_loc
+	state.linear_velocity = velocity_vector.rotated(rotation)
 	
 	# --- 5. DAMPING & ENVIRONMENT ---
 	state.angular_velocity *= 0.92
